1ffc8545e5f35bc6c0d3773e6903c817975932ba
Nazareno <nazareno@computacao.ufcg.edu.br>
2016-04-09 20:39:36 -0300

    json com todas as cidades pro teste

--- public/data/similaridade_cidades.R
+++ public/data/similaridade_cidades.R
@@ -1,16 +1,50 @@
 require("proxy")
+library("FastKNN")
 
-dados2010 <- read.csv("/home/iara/diferentonas-server/public/data/dados2010.csv", header=T)
+acha_vizinhos = function(dist10, k, dados2010){
+  n = dim(dist10)[1]
+  nn = matrix(0, n, k) # n x k
+  for (i in 1:n)
+    nn[i,] = k.nearest.neighbors(i, dist10, k = k)
+  df = as.data.frame(nn)
+  neighours = df %>% 
+    mutate(n1 = dados2010$cod7[V1], 
+           n1.nome = paste(dados2010$municipio[V1], dados2010$UF[V1]),
+           n2 = dados2010$cod7[V2], 
+           n2.nome = paste(dados2010$municipio[V2], dados2010$UF[V2]),
+           n3 = dados2010$cod7[V3], 
+           n3.nome = paste(dados2010$municipio[V3], dados2010$UF[V3]),
+           n4 = dados2010$cod7[V4], 
+           n5 = dados2010$cod7[V5], 
+           n6 = dados2010$cod7[V6])
+  neighours$origem = dados2010$cod7
+  neighours$origem.nome = paste(dados2010$municipio, dados2010$UF)
+  neighours
+}
 
-dados2010$pop <- (dados2010$pop - min(dados2010$pop))/(max(dados2010$pop) - min(dados2010$pop))
+dados2010 <- read.csv("public/data/dados2010.csv", header=T)
 
+quantitativos = dados2010 %>% 
+  select(6:9) %>% 
+  mutate_each(funs(scale))
+
+k=6
+  
 #distancia euclidiana
-dist10 <- dist(dados2010[,3:7], method="euclidean")
-df <- data.frame(as.matrix(dist10), row.names = dados2010$cod)
-colnames(df) = dados2010$cod
+dist10 <- as.matrix(dist(quantitativos, method="euclidean"))
+vizinhos.euclides = acha_vizinhos(dist10, k, dados2010)
 
 #distancia com cos
-dist10 <- dist(dados2010[,3:7], method="cosine")
-df <- data.frame(as.matrix(dist10), row.names = dados2010$cod)
-colnames(df) = dados2010$cod
+dist10.cos <- as.matrix(dist(quantitativos, method="cosine"))
+vizinhos.cosseno = acha_vizinhos(dist10.cos, k, dados2010)
 
+vizinhos.euclides %>% 
+  select(7, 9, 11, 13:16, 17, 8, 10, 12) %>% 
+  write.csv("public/data/vizinhos.euclidiano.csv", row.names = FALSE)
+
+vizinhos.cosseno %>% 
+  select(7, 9, 11, 13:16, 17, 8, 10, 12) %>% 
+  write.csv("public/data/vizinhos.cosseno.csv", row.names = FALSE)
+
+library(jsonlite)
+toJSON(select(neighours, origem.nome, origem))
