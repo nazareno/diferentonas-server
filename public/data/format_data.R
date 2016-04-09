420f0d074293e2990363f9c1fad5fd4366cbfc7f
Nazareno <nazareno@computacao.ufcg.edu.br>
2016-04-09 19:24:47 -0300

    Mantém códigos de 6 e 7 dígitos pros municípios

--- public/data/format_data.R
+++ public/data/format_data.R
@@ -7,12 +7,14 @@ idhm[,1]<-as.character(idhm[,1])
 idhm$UF <- substr(idhm[,1],nchar(idhm[,1])-2,nchar(idhm[,1])-1)
 idhm[,1] <- substr(idhm[,1],1,nchar(idhm[,1])-5)
 
+
 populacao <- read.csv("populacao.csv", header=T,sep=";")
 colnames(populacao) <- c("UF","COD.IBGE","Lugar","POP1991","POP1996","POP2000","POP2007","POP2010","x")
 
+populacao$codigo.original = populacao$COD.IBGE
 populacao$COD.IBGE <- substr(populacao$COD.IBGE, 1, nchar(populacao$COD.IBGE)-1)
 
-merge_data <- merge(idhm, populacao, by="COD.IBGE")
+merge_data <- merge(idhm, populacao, by=("COD.IBGE"))
 
 idhm91 <- as.data.frame(cbind(merge_data$Lugar,merge_data$COD.IBGE.x,
                               merge_data$IDHM..1991.,merge_data$IDHM.Renda..1991.,merge_data$IDHM.Longevidade..1991.,
@@ -26,10 +28,21 @@ idhm00 <- as.data.frame(cbind(merge_data$Lugar,merge_data$COD.IBGE.x,
 
 colnames(idhm00) <- c("municipio","cod","idhm","idhm_renda","idhm_longev","idhm_edu","pop")
 
-idhm10 <- as.data.frame(cbind(as.character(merge_data$COD.IBGE),merge_data$Lugar.x,
-                              merge_data$IDHM..2000.,merge_data$IDHM.Renda..2010.,merge_data$IDHM.Longevidade..2010.,
-                              merge_data$IDHM.Educação..2010.,merge_data$POP2010))
+idhm10 = merge_data %>% 
+  select(COD.IBGE, codigo.original, Lugar.x, UF.x, 
+         IDHM..2010., IDHM.Renda..2010., IDHM.Longevidade..2010., IDHM.Educação..2010., 
+         POP2010) %>% 
+  mutate(COD.IBGE = as.character(COD.IBGE), 
+         codigo.original = as.character(codigo.original))
 
-colnames(idhm10) <- c("cod","municipio","idhm","idhm_renda","idhm_longev","idhm_edu","pop")
+names(idhm10) <- c("cod6", 
+                      "cod7", 
+                      "municipio", 
+                      "UF", 
+                      "idhm",
+                      "idhm_renda",
+                      "idhm_longev",
+                      "idhm_edu",
+                      "pop")
 
 write.csv(idhm10,"dados2010.csv",row.names = F)
