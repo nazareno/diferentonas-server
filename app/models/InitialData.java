4d3cd85053c126ae9c401387d78119100eec0939
Ricardo Araújo <ricoaraujosantos@gmail.com>
2016-04-10 15:27:51 -0300

    adicionando cidades similares

--- app/models/InitialData.java
+++ app/models/InitialData.java
@@ -4,8 +4,9 @@ import java.io.File;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import javafx.print.PageLayout;
 import org.h2.tools.Csv;
 
 import play.Logger;
@@ -14,28 +15,21 @@ import play.db.jpa.JPAApi;
 
 import com.google.inject.Inject;
 
-import static play.api.Play.*;
-
 public class InitialData {
 
     @Inject
 	public InitialData(Environment environment, JPAApi jpaAPI) {
 
 		List<Cidade> cidades = jpaAPI.withTransaction(entityManager -> {
-			return entityManager.createQuery("FROM Cidade").getResultList();
+			return entityManager.createQuery("FROM Cidade", Cidade.class).getResultList();
 		});
 
 
 		if (cidades.isEmpty()) {
             Logger.info("Populando BD");
-			try {
+            try {
 
-                String dataPath = null;
-                if(new File("public").exists()){
-                    dataPath = "public/data/dados2010.csv";
-                } else {
-                    dataPath = "data/dados2010.csv";
-                }
+                String dataPath = new File("public").exists()? "public/data/dados2010.csv": "data/dados2010.csv";
 
                 ResultSet resultSet = new Csv().read(dataPath, null, "utf-8");
 				resultSet.next(); // header
@@ -58,10 +52,40 @@ public class InitialData {
                         Logger.info("Inseri " + count + " cidades...");
                     }
 				}
+				
+				dataPath = new File("public").exists()? "public/data/vizinhos.euclidiano.csv": "data/vizinhos.euclidiano.csv";
+
+                resultSet = new Csv().read(dataPath, null, "utf-8");
+				resultSet.next(); // header
+                count = 0;
+				while (resultSet.next()) {
+					long originID = resultSet.getLong(12);
+					Cidade origin = jpaAPI.withTransaction(em -> em.find(Cidade.class, originID));
+					
+					List<Cidade> similares = IntStream
+							.range(13, 23)
+							.mapToObj(
+									value -> jpaAPI.withTransaction(em -> em
+											.find(Cidade.class, value)))
+							.map(obj -> (Cidade) obj)
+							.collect(Collectors.toList());
+					
+					origin.setSimilares(similares);
+					jpaAPI.withTransaction(() -> {
+						jpaAPI.em().persist(origin);
+					});
+                    count++;
+                    if(count % 500 == 0){
+                        Logger.info("Inseri " + count + " cidades...");
+                    }
+				}
 			} catch (SQLException e1) {
                 Logger.error(e1.getLocalizedMessage());
                 e1.printStackTrace();
 			}
+			
+			
+			
 		} else {
             Logger.info("BD já populado");
         }
