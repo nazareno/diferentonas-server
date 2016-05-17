930a6aa2292a2f8abf94756600b6e59db95ee752
Ricardo Araújo <ricoaraujosantos@gmail.com>
2016-05-17 01:58:47 -0300

    fiz o melhor que pude com os testes nesse play... te contar, viu

--- test/WithBrowserTest.java
+++ test/WithBrowserTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 
 import play.test.WithBrowser;
 
+/**
+ * 
+ * 
+ * @author ricardoas
+ */
 public class WithBrowserTest extends WithBrowser{
 
     /**
@@ -15,6 +20,12 @@ public class WithBrowserTest extends WithBrowser{
     public void test() {
     	browser.goTo("/");
     	assertThat(browser.pageSource(), containsString("Olar"));
+    }
+
+    @Test
+    public void testCidadeInexistente() {
+    	browser.goTo("/cidade/0");
+    	assertThat(browser.pageSource(), containsString("Not found 0"));
     }
 
 }
--- test/controllers/CidadeControllerTest.java
+++ test/controllers/CidadeControllerTest.java
@@ -1,31 +1,46 @@
-import controllers.CidadeController;
+package controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static play.mvc.Http.Status.NOT_FOUND;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.contentAsString;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Result;
 import play.test.WithApplication;
-import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.*;
-
-import static org.junit.Assert.*;
 
 /**
  * Testes do controller.
  */
 public class CidadeControllerTest extends WithApplication {
+	
+	@Override
+	protected Application provideApplication() {
+		return new GuiceApplicationBuilder().build();
+	}
 
-    @Override
-    protected Application provideApplication() {
-        return new GuiceApplicationBuilder()
-                .build();
-    }
+	@Test
+	public void testIndex() {
+		Result result = new CidadeController().index();
+		assertEquals(OK, result.status());
+		assertEquals("text/plain", result.contentType().get());
+		assertEquals("utf-8", result.charset().get());
+		assertTrue(contentAsString(result).contains("Olar"));
+	}
 
-    @Test
-    public void testIndex() {
-        Result result = new CidadeController().index();
-        assertEquals(OK, result.status());
-        assertEquals("text/plain", result.contentType().get());
-        assertEquals("utf-8", result.charset().get());
-        assertTrue(contentAsString(result).contains("Olar"));
-    }
+	/**
+	 * Test method for {@link controllers.CidadeController#getCidades()}.
+	 * FIXME and remove {@link Ignore}. Maybe we should mock and not inject...
+	 */
+	@Test
+	@Ignore("Not working without injecting JPAApi and I don't know why...")
+	public void testGetCidadeInexistente() {
+		Result result = new CidadeController().get(0L);
+		assertEquals(NOT_FOUND, result.status());
+	}
 }
