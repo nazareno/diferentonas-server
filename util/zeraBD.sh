c68c17ed292873de802088526d20d712df3f9f7e
Luiz Augusto Morais <luizaugustomm@gmail.com>
2016-05-18 20:47:19 -0300

    Modificado script para zerar o bd local

--- util/zeraBD.sh
+++ util/zeraBD.sh
@@ -3,6 +3,9 @@
 set -e
 
 # Destroi e cria um BD local limpo.
+db_name="diferentonas"
 
-psql -c "drop database diferentonas;"
-psql -c "create database diferentonas;"
+dropdb $db_name
+createdb $db_name
+
+echo $db_name "database is clean!"
