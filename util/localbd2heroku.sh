47e3c633a0a77bc85cb164d152f1b513369ef99c
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-05-18 18:52:43 -0300

    Cria script para espelhar seu BD no heroku

---
+++ util/localbd2heroku.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# DESTRÓI BD no Heroku e o substitui pelo BD 'diferentonas' local.
+# Isso é útil para acelerar o processo de deploy no heroku,
+# já que popular um BD vazio lá com cidades e iniciativas
+# demora mais de 60s, o que faz com que o heroku cancele o deploy.
+
+set -e
+
+echo "DESTRUINDO BD atual no Heroku e enviando o local para lá"
+heroku pg:reset DATABASE
+heroku pg:push diferentonas DATABASE_URL --app diferentonas
+
+echo "Reiniciando todos os dynos"
+heroku restart
