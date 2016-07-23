67ef16d526174075e1c73f9d9e206aa18579db91
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-07-23 12:35:07 -0300

    Passa a usar um logger para R (#16)

--- R/atualiza_dados_cli.R
+++ R/atualiza_dados_cli.R
@@ -13,12 +13,14 @@ arquivo_diferentices = args[5]
 arquivo_historicos = args[6]
 
 # Debug / uso interativo: 
-# dados_siconv = "dados-externos/siconv/01_ConveniosProgramas-20160627.csv"
+# dados_siconv = "dados-externos/siconv-Xxxxx"
 # arquivo_siafi = "dados-externos/convenios-siafi-201607.csv"
 # arquivo_vizinhos = "dist/data/vizinhos.euclidiano.csv"
-# arquivo_iniciativas_saida = "dist/data/iniciativas-20160705.csv"
-# arquivo_diferentices = "dist/data/diferentices-20160705.csv"
-# arquivo_historicos = "dist/data/historico-20160705.csv"
+# arquivo_iniciativas_saida = "dist/data/iniciativas-20160722.csv"
+# arquivo_diferentices = "dist/data/diferentices-20160722.csv"
+# arquivo_historicos = "dist/data/historico-20160722.csv"
+
+library(futile.logger)
 
 # -----------------------------------------------------------
 # Organiza os dados do SICONV
@@ -27,7 +29,7 @@ source("R/consolida_siconv.R")
 convenios = consolida_convenios(dados_siconv)
 historico = carrega_e_limpa_historicos(dados_siconv, convenios$NR_CONVENIO)
 write.csv(historico, arquivo_historicos, row.names = FALSE)
-message("Históricos dos convênios salvo em ", arquivo_historicos)
+flog.info("Históricos dos convênios salvo em %s", arquivo_historicos)
 
 # -----------------------------------------------------------
 # Cruza dados do SICONF + SIAFI com dados sobre os municípios:
@@ -37,16 +39,16 @@ source("R/traducao_termos.R")
 iniciativas = cruza_dados(convenios, arquivo_siafi)
 iniciativas = traduz_termos(iniciativas)
 write.csv(iniciativas, arquivo_iniciativas_saida, row.names = FALSE)
-message("Dados de iniciativa salvos em ", arquivo_iniciativas_saida)
+flog.info("Dados de iniciativa salvos em %s", arquivo_iniciativas_saida)
 
 # -----------------------------------------------------------
 # Calcula scores de diferentices das cidades
 # -----------------------------------------------------------
 source("R/diferentices.R")
-message("Calculando diferentices a partir de ", arquivo_iniciativas_saida, " e ", arquivo_vizinhos)
+flog.info("Calculando diferentices a partir de %s e %s", arquivo_iniciativas_saida, arquivo_vizinhos)
 diferentices = calcula_diferentices(arquivo_iniciativas_saida, arquivo_vizinhos)
 write.csv(diferentices,
           arquivo_diferentices,
           row.names = FALSE)
-message("Diferentices salvas em ", arquivo_diferentices)
+flog.info("Diferentices salvas em %s", arquivo_diferentices)
 
