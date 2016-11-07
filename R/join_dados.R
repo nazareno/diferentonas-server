d7db7a4326bf06ae3b86989ad3642b0376d1abde
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-11-07 10:27:19 -0300

    Inclui no BD convênios com o DF para incluir BSB

--- R/join_dados.R
+++ R/join_dados.R
@@ -9,10 +9,13 @@ filtra_convenios = function(convenios) {
   futile.logger::flog.info("Antes de filtrar: %d convênios", NROW(convenios))
   
   ## seleciona e salva apenas os convênios propostos no âmbito municipal
-  futile.logger::flog.info("Filtrando aqueles depois de 2013 e celebrados por municípios")
+  futile.logger::flog.info("Filtrando aqueles depois de 2013 e celebrados por municípios + Brasília")
   t = convenios %>%
     filter(ANO_PROP >= 2013,
-           NATUREZA_JURIDICA == "Administração Pública Municipal") %>%
+           (NATUREZA_JURIDICA == "Administração Pública Municipal") |
+             ((MUNIC_PROPONENTE == "BRASILIA") & # Os convênios de Brasília não são municípios
+                (NATUREZA_JURIDICA == "Administração Pública Estadual ou do Distrito Federal")
+             )) %>%
     select(-NATUREZA_JURIDICA)
   
   futile.logger::flog.info(paste("Filtrados são ", NROW(convenios), "convênios"))
