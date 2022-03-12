ade331d606fad0047a73f3ef76299d8ebc5d6871
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-13 01:36:35 +0800

    Delete dados-externos directory

--- dados-externos/get_dados_coordenadas.sh
+++
@@ -1,4 +0,0 @@
-#wget ftp://geoftp.ibge.gov.br/organizacao_territorial/localidades/Google_KML/BR%20Localidades%202010%20v1.kml -O coordenadas.kml
-echo "\"COD.IBGE\",\"LONGITUDE\",\"LATITUDE\",\"ALTITUDE\"" > coordenadas.csv
-awk -F '[<>]' '$2~/CD_CATEGORIA|CD_GEOCODMU|coordinates/ {print $3}' coordenadas.kml | tail -n+17 | paste -d ',' - - - | awk -F ',' 'BEGIN { OFS = ","} $2 == "05" {print $1, $3, $4, $5}' >> coordenadas.csv
-rm *.kml
